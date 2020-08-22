7df274cb38ba9be996dca4a8bc69698732e23e1f
juan carlos buendia prieto <juan.carlosutninfo@gmail.com>
2020-08-22 16:32:33 -0500

    son cambios originarios de branch/cursogit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function PruebaGit(params) {
     console.log("hola mundo");
     console.log("esto es una modificacion desde otro lado")
+    console.log("esto es una modificacion de la branch de cursogit")
 }
